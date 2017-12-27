cd87409889200d6ab6eac822038d3a3fb6ea9279
Joran van Apeldoorn <jorants@gmail.com>
2017-12-27 14:26:18 +0100

    XFCE optional

--- archlinux.sh
+++ archlinux.sh
@@ -2162,6 +2162,9 @@ echo -e "Kernel Arch is: $chromebook_arch  Installing ArchLinuxARM Arch: ${archl
 
 read -p "Press [Enter] to continue..."
 
+read -p "Install XFCE? (y/N) " -n 1 -r
+echo    # (optional) move to a new line
+
 if [ ! -d /mnt/stateful_partition/archlinux ]
 then
   mkdir /mnt/stateful_partition/archlinux
@@ -2210,7 +2213,11 @@ install_dev_tools
 
 install_xbase
 
-install_xfce4
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    install_xfce4
+fi
+
 
 install_sound
 
